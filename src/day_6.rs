929cac568f2a0eb381a490a0324851a988366da9
Carbonhell <frafonia@gmail.com>
2025-12-09 00:35:20 +0100

    feat(day-6): solve second half

--- src/day_6.rs
+++ src/day_6.rs
@@ -6,7 +6,13 @@ use std::io::Lines;
 pub fn day_6_solution(input: Lines<impl std::io::BufRead>) {
     let lines = input.map_while(Result::ok).collect::<Vec<_>>();
     let lines = lines.iter().map(|x| x.as_str()).collect::<Vec<_>>();
-    let problems = Problem::from_table(lines);
+    let problems = Problem::from_table_l2r(&lines);
     let sum = problems.iter().map(|x| x.solve()).sum::<i64>();
-    println!("Grand total of all the problems: {}", sum);
+    println!("Grand total of all the problems (normal math): {}", sum);
+    let problems_r2l = Problem::from_table_r2l(&lines);
+    let sum_r2l = problems_r2l.iter().map(|x| x.solve()).sum::<i64>();
+    println!(
+        "Grand total of all the problems (right-to-left): {}",
+        sum_r2l
+    );
 }
--- src/day_6/problem.rs
+++ src/day_6/problem.rs
@@ -1,7 +1,8 @@
 use std::str::FromStr;
+use tracing::debug;
 
 /// Describes both operators and operands (i32s)
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 enum Op {
     Mul,
     Add,
@@ -33,7 +34,7 @@ impl Problem {
     /// It is assumed that each column has an equal amount of numbers, with the last element being an
     /// operation, and that each column is separated by an arbitrary amount of spaces.
     /// The result will be a mathematical expression with a series of operands followed by a single operator to apply to them.
-    pub fn from_table(table: Vec<&str>) -> Vec<Problem> {
+    pub fn from_table_l2r(table: &Vec<&str>) -> Vec<Problem> {
         let x = table
             .iter()
             .map(|x| {
@@ -54,6 +55,76 @@ impl Problem {
                     .collect::<Vec<_>>(),
             })
             .collect();
+
+        problems
+    }
+
+    /// Builds a vec of problems from a matrix, reading from right to left and considering number positions in each column.
+    ///
+    /// SAFETY:
+    /// It is assumed that each column may only contain spaces at its start or at the end, but not between digits.
+    /// If a space is present between digits, any digit above the space *will* be ignored, as it is unclear how such case should be handled from the instructions.
+    pub fn from_table_r2l(table: &Vec<&str>) -> Vec<Problem> {
+        let columns = table[0].len();
+        let mut iter_of_chars = table.iter().map(|x| x.chars().rev()).collect::<Vec<_>>();
+
+        // Transpose the vec of vecs to get columns instead of rows - shamelessly taken from https://stackoverflow.com/questions/64498617/how-to-transpose-a-vector-of-vectors-in-rust
+        // It is safe to assume each row will have the same amount of columns, as the amount of columns is equal to the amount of problems we have
+        let mut skip_delimiter_column = false;
+        let mut problems = vec![];
+        let orphan_operands: Vec<_> = (0..columns).fold(vec![], |mut acc, col_idx| {
+            debug!(%col_idx, "Processing column");
+            let mut column = iter_of_chars
+                .iter_mut()
+                .map(|n| {
+                    n.next().expect(
+                        "Couldn't get next column - ensure all rows have the same amount of chars",
+                    )
+                })
+                // At this point, we'll have an iterator of chars of the column being processed, and we need to convert it back to an operand
+                // to_string can be optimized - we just need it because we're dealing with chars
+                .rev(); // Start from the bottom cell, either an operator or a space, followed by the operand digits
+            if skip_delimiter_column {
+                // Process the column by just skipping it
+                column.for_each(|_| {});
+                skip_delimiter_column = false;
+                return acc;
+            }
+            // Store the operand temporarily, to use it later on to decide whether to fold the accumulator or continue to the next column
+            let operator = column
+                .next()
+                .expect("Unexpected end of column while parsing operator/operand");
+            let operand = column
+                .filter(|x| x != &' ')
+                .map(|x| {
+                    x.to_digit(10)
+                        .expect("Unexpected non-digit character found while parsing operand")
+                        as i64
+                })
+                .enumerate()
+                .fold(0, |acc, (i, digit)| acc + (digit * 10_i64.pow(i as u32)));
+            debug!(?operator, ?operand, "Pushing operand");
+            acc.push(Op::Expr(operand));
+            if operator != ' ' {
+                let op = operator
+                    .to_string()
+                    .parse::<Op>()
+                    .expect("Failed to parse operator");
+                debug!(?op, ?acc, "Found operator, flushing problem");
+                // Flush the accumulator
+                acc.push(op);
+                problems.push(Problem { ops: acc.clone() });
+                acc.clear();
+                // Signal to the next iteration that we need to skip the delimiter column
+                skip_delimiter_column = true;
+            }
+            acc
+        });
+        if !orphan_operands.is_empty() {
+            panic!(
+                "The table is not properly formatted. There are orphan operands that couldn't be folded into a valid problem."
+            );
+        }
 
         problems
     }
@@ -102,7 +173,7 @@ mod tests {
     #[test]
     fn test_problems_table_parsing() {
         let table = vec!["1 2 3 4", "5 6 7 8", "* + + *"];
-        let problems = Problem::from_table(table);
+        let problems = Problem::from_table_l2r(&table);
         assert_eq!(
             problems,
             vec![
@@ -125,10 +196,38 @@ mod tests {
     #[test]
     fn test_problems_solving() {
         let table = vec!["1 2 3 4", "5 6 7 8", "* + + *"];
-        let problems = Problem::from_table(table);
+        let problems = Problem::from_table_l2r(&table);
         assert_eq!(
             problems.iter().map(|p| p.solve()).collect::<Vec<_>>(),
             vec![5, 8, 10, 32]
         );
+    }
+
+    #[test]
+    fn test_problems_table_parsing_r2l_example() {
+        let table = vec![
+            "123 328  51 64 ",
+            " 45 64  387 23 ",
+            "  6 98  215 314",
+            "*   +   *   +  ",
+        ];
+        let problems = Problem::from_table_r2l(&table);
+        assert_eq!(
+            problems,
+            vec![
+                Problem {
+                    ops: vec![Op::Expr(4), Op::Expr(431), Op::Expr(623), Op::Add]
+                },
+                Problem {
+                    ops: vec![Op::Expr(175), Op::Expr(581), Op::Expr(32), Op::Mul]
+                },
+                Problem {
+                    ops: vec![Op::Expr(8), Op::Expr(248), Op::Expr(369), Op::Add]
+                },
+                Problem {
+                    ops: vec![Op::Expr(356), Op::Expr(24), Op::Expr(1), Op::Mul]
+                }
+            ]
+        )
     }
 }
